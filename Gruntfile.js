72623e54db45d5fedaf87c062a2b275719d76e4d
Aaron Kosel <a_kosel@yahoo.com>
2014-09-27 12:09:17 -0400

    sassification. added better mobile view scaling

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,9 +4,18 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       files: ['public/js/*.js', '!public/js/*min.js']
+    },
+
+    sass: {
+      dist: {
+        files: {
+          'public/css/stylesheet.css': 'public/css/stylesheet.scss'
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.registerTask('default', 'jshint');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.registerTask('default', ['jshint', 'sass']);
 };
